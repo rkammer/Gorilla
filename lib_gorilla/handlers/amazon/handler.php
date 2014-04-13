7028e4dace8fe3f7b068202959b04d48f1b4e7b1
rodrigofmp <rodrigofmp@gmail.com>
2014-04-13 18:04:04 -0300

    ...

--- lib_gorilla/handlers/amazon/handler.php
+++ lib_gorilla/handlers/amazon/handler.php
@@ -27,67 +27,67 @@ use Aws\S3\S3Client;
  * @package		Joomla.Library
  * @subpackage	lib_gorilla
  */
-class GorillaHandlerAmazon extends GorillaHandler 
+class GorillaHandlerAmazon extends GorillaHandler
 {
 
 	/**
 	 * Upload file.
 	 *
 	 * @return true if success
-	 */	
+	 */
 	public function upload() {
 		echo "Uploaded=" . $this->_file['name'];
 		echo "GUID=" . $this->_guid;
-		
+
 		echo '1<br/>';
-		
-		
-		
+
+
+
 		echo '2<br/>';
-		
+
 		//cria o objecto
 		$s3Client = S3Client::factory(array(
-				'key'    => 'AKIAIWITUMDFORXDHRUA',
-				'secret' => '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',
+				'key'    => '',
+				'secret' => '',
 		));
-		
+
 		echo '3<br/>';
-		
+
 		//nome do bucket
 		$bucket_name = 'rkammer';
-		
+
 		echo '4<br/>';
-		
+
 		//cria o bucket (na primeira vez)
 		//$s3Client->createBucket(array('Bucket' => $bucket_name));
-		
+
 		echo '5<br/>';
-		
+
 		//cria um arquivo txt com o conteudo "Hello World"
 		//$result = $s3Client->putObject(array(
 		//		'Bucket' => $bucket_name,
 		//		'Key'    => 'data.txt',
 		//		'Body'   => 'Hello World!!'
 		//));
-		
+
 		$s3Client->putObject(array(
 				'Bucket'     => $bucket_name,
 				'Key'        => $this->_guid,
 				'SourceFile' => $this->_file['tmp_name'],
 				'ACL'        => 'public-read'
-		));		
-		
-		echo "banana";		
-		
+		));
+
+		echo "banana";
+
 	}
-	
+
 	/**
 	 * Prepare file and get link to download.
 	 *
 	 * @return string Full download link
 	 */
 	public function getDownloadLink() {
-	
-	}	
-	
+
+	}
+
 }
