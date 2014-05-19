d00b3cda5cf6f2364c671e4eb13f6cd87a33d7d9
rodrigofmp <rodrigofmp@gmail.com>
2014-05-18 21:08:48 -0300

    0.0.8.sql was missing.

--- com_gorilla/admin/sql/updates/mysql/0.0.8.sql
+++ com_gorilla/admin/sql/updates/mysql/0.0.8.sql
@@ -1,34 +1,37 @@
+    -- -----------------------------------------------------
+	-- Table `#__gorilla_notes`
 	-- -----------------------------------------------------
-	-- Adds max file size and dropped dir config
-	-- -----------------------------------------------------
-	INSERT INTO `#__gorilla_config`
-	(`key`, `value`)
-	SELECT 'MAX_FILE_SIZE',''
-	  FROM dual
-	 WHERE not exists (SELECT 1 AS OK
-	                     FROM `#__gorilla_config`
-	                    WHERE `key` = 'MAX_FILE_SIZE' );
-	                    
-	INSERT INTO `#__gorilla_config`
-	(`key`, `value`)
-	SELECT 'DROPPED_DIR',''
-	  FROM dual
-	 WHERE not exists (SELECT 1 AS OK
-	                     FROM `#__gorilla_config`
-	                    WHERE `key` = 'DROPPED_DIR' );
-	                    
-	INSERT INTO `#__gorilla_config`
-	(`key`, `value`)
-	SELECT 'STORAGE',''
-	  FROM dual
-	 WHERE not exists (SELECT 1 AS OK
-	                     FROM `#__gorilla_config`
-	                    WHERE `key` = 'STORAGE' );
-	                    
-	INSERT INTO `#__gorilla_config`
-	(`key`, `value`)
-	SELECT 'LOCAL_DIR',''
-	  FROM dual
-	 WHERE not exists (SELECT 1 AS OK
-	                     FROM `#__gorilla_config`
-	                    WHERE `key` = 'LOCAL_DIR' );
+	CREATE TABLE IF NOT EXISTS `#__gorilla_notes` (
+	  `id` 					INT NOT NULL AUTO_INCREMENT,
+	  `title` 				VARCHAR(255) 		NOT NULL,
+	  `alias` 				VARCHAR(255) 		NOT NULL,
+	  `description` 		MEDIUMTEXT 			NULL,
+	  `container_id` 		INT 				NOT NULL,
+	  `published` 			TINYINT(3) 			NOT NULL DEFAULT 0,
+	  `access` 				INT(10) 			NOT NULL,
+	  `ordering` 			INT(11) 		 	NOT NULL,
+	  `checked_out` 		INT(11) UNSIGNED 	NOT NULL DEFAULT 0,
+	  `checked_out_time` 	DATETIME 			NOT NULL DEFAULT '0000-00-00 00:00:00',
+	  `metadesc` 			VARCHAR(1024) 		NULL,
+	  `metakey` 			VARCHAR(1024) 		NULL,
+	  `created` 			DATETIME 			NOT NULL DEFAULT '0000-00-00 00:00:00',
+	  `created_by` 			INT(10) 			NOT NULL,
+	  `modified` 			DATETIME 			NOT NULL DEFAULT '0000-00-00 00:00:00',
+	  `modified_by` 		INT(10) 			NULL,
+	  `publish_up` 			DATETIME 			NOT NULL DEFAULT '0000-00-00 00:00:00',
+	  `publish_down` 		DATETIME 			NOT NULL DEFAULT '0000-00-00 00:00:00',
+	  `asset_id` 			INT(10) 			NOT NULL DEFAULT '0',
+	  `guid`                VARCHAR(36)      	NOT NULL,
+	  PRIMARY KEY (`id`),
+	  INDEX `idx_access` (`access` ASC),
+	  INDEX `idx_checkout` (`checked_out` ASC),
+	  INDEX `idx_created_by` (`created_by` ASC),
+	  INDEX `idx_published` (`published` ASC),
+	  INDEX `idx_title` (`title` ASC),
+	  INDEX `fk_notes_containers_idx` (`container_id` ASC),
+	  CONSTRAINT `fk_notes_containers`
+	    FOREIGN KEY (`container_id`)
+	    REFERENCES `#__gorilla_containers` (`id`)
+	    ON DELETE NO ACTION
+	    ON UPDATE NO ACTION)
+	ENGINE = InnoDB;
